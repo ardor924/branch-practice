e4a05ebfcce1a325657a5d459548b4d1e00f26a0
ardor924 <ardor924@gmail.com>
2023-09-18 12:19:36 +0900

    feat : print buzz when multiple of 5 is inputed

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+def buzz(num:int) :
+    for i in range(1,num+1) :
+        if i % 5 == 0 :
+            print('buzz')
+        else :
+            print(i)
+
+if __name__ == '__main__' :
+    buzz(15)
