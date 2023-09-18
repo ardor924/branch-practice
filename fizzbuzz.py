ac2d6492eb144bbf2680c32264d8c2ad572c4c1d
ardor924 <ardor924@gmail.com>
2023-09-18 12:20:58 +0900

    feat : print fizz when multiple of 3 is inputed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 def fizz(num:int) :
-    for i in range(1,num+1) :
+    for i in range(1,num) :
         if i % 3 == 0 :
             print('fizz')
         else :
             print(i)
 
 if __name__ == '__main__' :
-    fizz(15)
+    fizz(15+1)
